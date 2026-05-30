a6576006642eae01ff6f5cc5b1414bc32d846607
Behruz141 <gayratovbehruz98@gmail.com>
2026-05-30 11:24:02 +0500

    5 edit

--- src/layouts/header/Header.jsx
+++ src/layouts/header/Header.jsx
@@ -26,7 +26,7 @@ const Header = () => {
           </div>
 
           {/* Right Actions */}
-          <div className="flex items-center gap-5 md:gap-10 shrink-0 pl-8">
+          <div className="flex items-center gap-5 md:gap-10 shrink-0 pl-6">
             <ThemeToggle />
             <LanguageSwitcher />
             {/* Mobile Menu */}
